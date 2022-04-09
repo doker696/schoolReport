649fec079d204d8a6ab703e4b95c48a387d390a4
Kirill Tokarev <kirill.tokarev@osinit.com>
2022-04-10 00:58:44 +0300

    change git folder

---
+++ server/modules/db.pm
@@ -0,0 +1,24 @@
+package DB;
+use strict;
+use warnings;
+use Exporter;
+use DBI;
+
+our @ISA = qw( Exporter );
+
+# these are exported by default.
+our @EXPORT = qw( db_connect );
+
+sub db_connect {
+    my $driver   = "Pg";
+    my $database = "perlKurs";
+    my $dsn    = "DBI:$driver:dbname = $database;host = 127.0.0.1;port = 5432";
+    my $userid = "postgres";
+    my $password = "1";
+    my $dbh      = DBI->connect( $dsn, $userid, $password, { RaiseError => 1 } )
+      or die $DBI::errstr;
+
+    return ($dbh);
+};
+
+1;
